5ffee1b4aaf019e24e1753be8c3c32eeabe8fc1a
ezn6 <kcourtney@naver.com>
2022-03-07 10:50:22 +0900

    validation check, middleware for validation

--- router/post.js
+++ router/post.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweets.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
 // DB없이 메모리로 서버 구성하는 version
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text should be at least 3 characters'),
+  validate,
+];
 
 //전체 트윗 조회 api + 특정 유저 트윗 조회
 router.get('/', tweetController.getTweets);
@@ -13,10 +23,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 //트윗 작성
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 //트윗 수정
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 //트윗 삭제
 router.delete('/:id', tweetController.removeTweet);
